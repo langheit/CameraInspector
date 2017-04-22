e39ad66ec86ebb20e47b8b8a141f6256a771f39e
Langheit <christian.langheit@sympatico.ca>
2017-04-22 15:11:13 -0400

    hybride

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,3 +1,49 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    takePicture: function() {
+      navigator.camera.getPicture( function( imageURI ) {
+        alert( imageURI );
+      },
+      function( message ) {
+        alert( message );
+      },
+      {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI
+      });
+    }
+};
+
+/*
 var myApp = new Framework7({
     material: isIos? false : true
 //  template7Pages: true,
@@ -54,7 +100,7 @@ myApp.onPageInit('anomalie', function (page) {
     // Do something here for "about" page
 //    myApp.alert('Here comes anomali page');
 })
-
+*/
 
 
 /*
@@ -101,12 +147,13 @@ myApp.onPageInit('details', function(page) {
 
 */
 function getImage() {
+      alert("get image");
       navigator.camera.getPicture( function( imageURI ) {
-        myApp.alert(imageURI);
+//        myApp.alert(imageURI);
         alert( imageURI );
       },
       function( message ) {
-        myApp.alert("get picture failed");
+//       myApp.alert("get picture failed");
         alert( message );
       },
       {
